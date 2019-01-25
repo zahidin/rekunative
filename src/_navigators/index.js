7df13a9bc1649dee7a6c7cd83aa802f5d3cd11a7
Animeflix <animeflix.id@gmail.com>
2019-01-25 15:15:37 +0700

    add signin2 and forgotpassword

--- src/_navigators/index.js
+++ src/_navigators/index.js
@@ -1,26 +1,43 @@
-import {Home, Login, TodoList, TodoAdd,Signin1} from '../screens'
-import {createStackNavigator} from 'react-navigation'
+import {
+	Home,
+	Login,
+	TodoList,
+	TodoAdd,
+	Signin1,
+	Signin2,
+	ForgotPassword
+} from '../screens';
+import { createStackNavigator } from 'react-navigation';
 
-const initialRouteName = "Signin1"
+const initialRouteName = 'Signin1';
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: Home    
-  },
-  Login: {
-    screen: Login    
-  },
-  Signin1: {
-    screen: Signin1    
-  },
-  TodoList: {
-    screen: TodoList    
-  },
-  TodoAdd: {
-    screen: TodoAdd
-  },
-}, {
-  initialRouteName
-});
+const AppNavigator = createStackNavigator(
+	{
+		Home: {
+			screen: Home
+		},
+		Login: {
+			screen: Login
+		},
+		Signin1: {
+			screen: Signin1
+		},
+		Signin2: {
+			screen: Signin2
+		},
+		ForgotPassword: {
+			screen: ForgotPassword
+		},
+		TodoList: {
+			screen: TodoList
+		},
+		TodoAdd: {
+			screen: TodoAdd
+		}
+	},
+	{
+		initialRouteName
+	}
+);
 
-export default AppNavigator
+export default AppNavigator;
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,13 +1,9 @@
-import Home from './home'
-import Login from './login'
-import TodoList from './todo-list'
-import TodoAdd from './todo-add'
-import Signin1 from './examples/signin1'
+import Home from './home';
+import Login from './login';
+import TodoList from './todo-list';
+import TodoAdd from './todo-add';
+import Signin1 from './examples/signin1';
+import Signin2 from './examples/signin2';
+import ForgotPassword from './examples/forgotpassword';
 
-export {
-    Home,
-    Login,
-    TodoList,
-    TodoAdd,
-    Signin1
-}
+export { Home, Login, TodoList, TodoAdd, Signin1, Signin2, ForgotPassword };
